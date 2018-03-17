c74eefe9b43c5caba139e9e1d0f5973118a8110d
Mark Johnson <mark@jhns.in>
2018-03-17 13:51:24 -0500

    Porting over pre-existing stuff from earlier work

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Hello World"
+    return render_template("index.html")
 
 
-app.run()
+@app.route('/login.html')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/profile/<name>')
+def profile(name):
+    return render_template("profile.html", name=name)
+
+
+if __name__ == "__main__":
+    app.run()
