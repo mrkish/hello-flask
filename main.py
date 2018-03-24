fc958b3c4e49b1651a0286faf9e167455c1f4f3a
Mark Johnson <mark@jhns.in>
2018-03-24 16:40:55 -0500

    Prepwork for class 5 and 6; Jinja; refactoring, etc

--- main.py
+++ main.py
@@ -3,42 +3,37 @@ import cgi
 import os
 import jinja2
 
+# Class 6 prepwork - Using Jinja2 Templates
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-time_form = """
-    <!doctype html>
-    <head>
-    <title>Validate Time</title>
-    </head>
-    <body>
-    <style>
-        .error {{ color: red; }}
-        </style>
-    <h1>Validate Time</h1>
-    <form method="POST">
-        <label>Hours (24-hour format)
-            <input name="hours" type="text" value="{hours}" />
-        </label>
-        <p class="error">{hours_error}</p>
-        <label>Minutes
-            <input name="minutes" type="text" value="{minutes}" />
-        </label>
-        <p class="error"> {minutes_error}</p>
-        <input type="submit" value="Validate" />
-    </form>
-    </body>
-    </html>
-    """
+
+@app.route('/')
+def index():
+    # Class 6 prepwork - Using Jinja2 Templates
+    template = jinja_env.get_template('/hello_form.html')
+    return template.render()
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    first_name = request.form['first_name']
+    # Class 6 prepwork - Using Jinja2 Templates
+    template = jinja_env.get_template('hello_greeting.html')
+    return template.render(name=first_name)
 
 @app.route('/validate-time')
 def display_time_form():
-    return time_form.format(hours='',hours_error='',minutes='', minutes_error='')
+    # Class 6 prepwork - Variable expressions
+    template = jinja_env.get_template('time_form.html')
+    return template.render()
 
 def is_integer(num):
     try:
         int(num)
-        return TrueP
+        return True
     except ValueError:
         return False
 
@@ -64,14 +59,15 @@ def validate_time():
     else:
         minutes = int(minutes)
         if minutes > 59 or minutes < 0:
-            miuntes_error = 'Minute value out of range (0-59)'
+            minutes_error = 'Minute value out of range (0-59)'
             minutes = ''
 
     if not minutes_error and not hours_error:
         time = str(hours) + ':' + str(minutes)
         return redirect('/valid-time?time={0}'.format(time))
-    else:
-        return time_form.format(hours_error=hours_error, 
+    else: # Class 6 prepwork - Variable expressions
+        template = jinja_env.get_template('time_form.html')
+        return template.render(hours_error=hours_error, 
                minutes_error=minutes_error, 
                hours = hours, 
                minutes = minutes)
@@ -80,6 +76,20 @@ def validate_time():
 def valid_time():
     time = request.args.get('time')
     return '<h1>You submitted {0}. Thanks for submitting a valid time!</h1>'.format(time)
+
+# Class 6 Prepwork -- Tasklist
+tasks = []
+
+@app.route('/todos', methods=['POST','GET'])
+def todos():
+
+    if request.method == "POST":
+        task = request.form['task']
+        tasks.append(task)
+
+    template = jinja_env.get_template('todos.html')
+    return template.render(title="TODOs",tasks=tasks) # passing page title var
+
 
 if __name__ == "__main__":
     app.run()
